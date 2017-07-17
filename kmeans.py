e1c5f0d0c2923815e973f1b39da9eec3f05c1aea
Ali Saeed <asalpha01@gmail.com>
2017-07-17 11:32:54 -0400

    included csv writer plus change in cluster size

--- kmeans.py
+++ kmeans.py
@@ -4,16 +4,41 @@ from nltk.corpus import stopwords
 from nltk.stem.wordnet import WordNetLemmatizer
 from sklearn.cluster import KMeans
 from sklearn.feature_extraction.text import TfidfVectorizer
+import csv
+
+
+#######################################################################################
+
+def read_csv(name, pos):
+    read_file = open(name + '.csv', 'r', encoding="latin1")
+    csv_read = csv.reader(read_file)
+    lst = []
+    for row in csv_read:
+        print("Extracting Data....")
+        lst.append(row[pos])
+    read_file.close()
+    return lst
 
 
 
-example = ['PWC cust connect frozen', 'PWC Cust connect frozen', 'PWC  Cust connect frozen', 'cust connect frozen',
+def writecsv(wfile, data):
+    for r in data:
+        print('Almost Done....')
+        wfile.writerow(r)
+        print('----------------')
+
+
+
+
+#######################################################################################
+example = ['Home Folder Please','PWC cust connect frozen', 'PWC Cust connect frozen', 'PWC  Cust connect frozen', 'cust connect frozen',
 'Cust connect frozen', 'PEC cust connect frozen', 'Pwc Cust connect frozen', 'PWC cust connect frozen tfsa not loading',
 'PWC cust connect frozen error 103','OC - unable to open file on network drive',
 'OCSC - missing network drive',
 'URGENT OC - unable to access files on network drive',
 'OC - Network drive not mapped',
 'USOC - windows - add network Drive',
+'Folder Please Create',
 'MI - Cannot Access Network Drive',
 'How to gain access to a network drive.',
 'M&I Connect - Map network drive and printer',
@@ -22,130 +47,11 @@ example = ['PWC cust connect frozen', 'PWC Cust connect frozen', 'PWC  Cust conn
 'request to map network drive',
 'user needs to be mapped to network drive',
 'request to map network drive',
+'Please Create Home',
 'How to get access to a shared network drive.',
 'How to get access to a shared network drive.',
-'MICROSOFT PATCH UPDATE CMRI70297350',
-'MICROSOFT PATCH UPDATE CMRI70297350',
-'MICROSOFT PATCH UPDATE CMRI70297357',
-'MICROSOFT PATCH UPDATE CMRI70297357',
-'MICROSOFT PATCH UPDATE CMRI70361938',
-'MICROSOFT PATCH UPDATE CMRI70361938',
-'MICROSOFT PATCH UPDATE CMRI70394042',
-'MICROSOFT PATCH UPDATE CMRI70394042',
-'MICROSOFT PATCH UPDATE CMRI70297352',
-'MICROSOFT PATCH UPDATE CMRI70297352',
-'MICROSOFT PATCH UPDATE CMRI70394086',
-'MICROSOFT PATCH UPDATE CMRI70394086',
-'MICROSOFT PATCH UPDATE CMRI70394048',
-'MICROSOFT PATCH UPDATE CMRI70394048',
-'MICROSOFT PATCH UPDATE CMRI70360996',
-'MICROSOFT PATCH UPDATE CMRI70360996',
-'MICROSOFT PATCH UPDATE CMRI70394045',
-'MICROSOFT PATCH UPDATE CMRI70394045',
-'MICROSOFT PATCH UPDATE CMRI70394055',
-'MICROSOFT PATCH UPDATE CMRI70394055',
-'MICROSOFT PATCH UPDATE CMRI70407062',
-'MICROSOFT PATCH UPDATE CMRI70407062',
-'MICROSOFT PATCH UPDATE CMRI70407083',
-'MICROSOFT PATCH UPDATE CMRI70407083',
-'MICROSOFT PATCH UPDATE CMRI70407114',
-'MICROSOFT PATCH UPDATE CMRI70407114',
-'MICROSOFT PATCH UPDATE CMRI70407182',
-'MICROSOFT PATCH UPDATE CMRI70407182',
-'MICROSOFT PATCH UPDATE CMRI70394083',
-'MICROSOFT PATCH UPDATE CMRI70394083',
-'MICROSOFT PATCH UPDATE CMRI70407108',
-'MICROSOFT PATCH UPDATE CMRI70407108',
-'MICROSOFT PATCH UPDATE CMRI70407187',
-'MICROSOFT PATCH UPDATE CMRI70407187',
-'MICROSOFT PATCH UPDATE CMRI70394041',
-'MICROSOFT PATCH UPDATE CMRI70394041',
-'MICROSOFT PATCH UPDATE CMRI70394087',
-'MICROSOFT PATCH UPDATE CMRI70394087',
-'MICROSOFT PATCH UPDATE CMRI70407055',
-'MICROSOFT PATCH UPDATE CMRI70407055',
-'MICROSOFT PATCH UPDATE CMRI70407074',
-'MICROSOFT PATCH UPDATE CMRI70407074',
-'MICROSOFT PATCH UPDATE CMRI70407110',
-'MICROSOFT PATCH UPDATE CMRI70407110',
-'MICROSOFT PATCH UPDATE CMRI70407183',
-'MICROSOFT PATCH UPDATE CMRI70407183',
-'MICROSOFT PATCH UPDATE CMRI70407192',
-'MICROSOFT PATCH UPDATE CMRI70407192',
-'MICROSOFT PATCH UPDATE CMRI70407193',
-'MICROSOFT PATCH UPDATE CMRI70407193',
-'MICROSOFT PATCH UPDATE CMRI70407194',
-'MICROSOFT PATCH UPDATE CMRI70407194',
-'MICROSOFT PATCH UPDATE CMRI70407239',
-'MICROSOFT PATCH UPDATE CMRI70407239',
-'MICROSOFT PATCH UPDATE CMRI70407212',
-'MICROSOFT PATCH UPDATE CMRI70407212',
-'MICROSOFT PATCH UPDATE CMRI70407209',
-'MICROSOFT PATCH UPDATE CMRI70407209',
-'MICROSOFT PATCH UPDATE CMRI70407210',
-'MICROSOFT PATCH UPDATE CMRI70407210',
-'MICROSOFT PATCH UPDATE CMRI70407211',
-'MICROSOFT PATCH UPDATE CMRI70407211',
-'MICROSOFT PATCH UPDATE CMRI70407216',
-'MICROSOFT PATCH UPDATE CMRI70407216',
-'MICROSOFT PATCH UPDATE CMRI70407218',
-'MICROSOFT PATCH UPDATE CMRI70407218',
-'MICROSOFT PATCH UPDATE CMRI70407237',
-'MICROSOFT PATCH UPDATE CMRI70407237',
-'MICROSOFT PATCH UPDATE CMRI70407414',
-'MICROSOFT PATCH UPDATE CMRI70407414',
-'MICROSOFT PATCH UPDATE CMRI70407415',
-'MICROSOFT PATCH UPDATE CMRI70407415',
-'MICROSOFT PATCH UPDATE CMRI70407170',
-'MICROSOFT PATCH UPDATE CMRI70407170',
-'MICROSOFT PATCH UPDATE CMRI70407422',
-'MICROSOFT PATCH UPDATE CMRI70407422',
-'MICROSOFT PATCH UPDATE CMRI70407423',
-'MICROSOFT PATCH UPDATE CMRI70407423',
-'MICROSOFT PATCH UPDATE CMRI70407206',
-'MICROSOFT PATCH UPDATE CMRI70407206',
-'MICROSOFT PATCH UPDATE CMRI70407246',
-'MICROSOFT PATCH UPDATE CMRI70407246',
-'MICROSOFT PATCH UPDATE CMRI70407249',
-'MICROSOFT PATCH UPDATE CMRI70407249',
-'MICROSOFT PATCH UPDATE CMRI70407252',
-'MICROSOFT PATCH UPDATE CMRI70407252',
-'MICROSOFT PATCH UPDATE CMRI70407409',
-'MICROSOFT PATCH UPDATE CMRI70407409',
-'MICROSOFT PATCH UPDATE CMRI70407417',
-'MICROSOFT PATCH UPDATE CMRI70407417',
-'MICROSOFT PATCH UPDATE CMRI70407424',
-'MICROSOFT PATCH UPDATE CMRI70407424',
-'MICROSOFT PATCH UPDATE CMRI70407461',
-'MICROSOFT PATCH UPDATE CMRI70407461',
-'MICROSOFT PATCH UPDATE CMRI70428274',
-'MICROSOFT PATCH UPDATE CMRI70428274',
-'MICROSOFT PATCH UPDATE CMRI70428355',
-'MICROSOFT PATCH UPDATE CMRI70428355',
-'MICROSOFT PATCH UPDATE CMRI70428408',
-'MICROSOFT PATCH UPDATE CMRI70428408',
-'MICROSOFT PATCH UPDATE CMRI70428211',
-'MICROSOFT PATCH UPDATE CMRI70428211',
-'MICROSOFT PATCH UPDATE CMRI70428380',
-'MICROSOFT PATCH UPDATE CMRI70428380',
-'MICROSOFT PATCH UPDATE CMRI70428414',
-'MICROSOFT PATCH UPDATE CMRI70428414',
-'MICROSOFT PATCH UPDATE CMRI70428465',
-'MICROSOFT PATCH UPDATE CMRI70428465',
-'MICROSOFT PATCH UPDATE CMRI70428484',
-'MICROSOFT PATCH UPDATE CMRI70428484',
-'MICROSOFT PATCH UPDATE CMRI70394069',
-'MICROSOFT PATCH UPDATE CMRI70394069',
-'MICROSOFT PATCH UPDATE CMRI70407035',
-'MICROSOFT PATCH UPDATE CMRI70407035',
-'MICROSOFT PATCH UPDATE CMRI70428542',
-'MICROSOFT PATCH UPDATE CMRI70428542',
-'MICROSOFT PATCH UPDATE CMRI70436679',
-'MICROSOFT PATCH UPDATE CMRI70436679',
-'MICROSOFT PATCH UPDATE CMRI70428200',
-'MICROSOFT PATCH UPDATE CMRI70428200',
-'MICROSOFT PATCH UPDATE CMRI70428252',
-'MICROSOFT PATCH UPDATE CMRI70428252']
+'Create Home Folder'
+]
 
 # tokenizer function
 def wrd_tknizer(sen):
@@ -164,7 +70,7 @@ def cluster_sentences(sntcs, no_clusters=5):
         tfidf_vectorizer = TfidfVectorizer(tokenizer=wrd_tknizer,
                                         stop_words=stopwords.words('english'),
                                         max_df=0.9,
-                                        min_df=0.1,
+                                        min_df=0,
                                         lowercase=True)
         tfidf_matrix = tfidf_vectorizer.fit_transform(sntcs)
         kmeans = KMeans(n_clusters=no_clusters)
@@ -177,20 +83,43 @@ def cluster_sentences(sntcs, no_clusters=5):
 
 def print_clusters(clust, sent):
     n = 0
+    new_data = []
     while n < nclusters:
-        title = example[clusters[n][0]]
+        title = example1[clusters[n][0]]
         lemmatized_title = lemmatizer.lemmatize(title)
-        print ("cluster ",n + 1,":", lemmatized_title)
+        #print ("cluster ",n + 1,":", lemmatized_title)
+        print ("cluster ",n + 1,":")
+        clstr = ''
+        val = 0
         for q,sentence in enumerate(clusters[n]):
-            print ("\tsentence ",q,": ",example[sentence]) 
+            main = example1[sentence]
+            val += int(value[sentence])
+            clstr += example1[sentence] + ', '
+            print ("\tsentence ",q,": ",example1[sentence])
+        print(val)
+        print(clstr)
         n += 1
-
+        new_data.append([val, main, clstr])
+    print(new_data)
+    return new_data
+example1 = read_csv('H:\\extra\\blah', 0)
+value = read_csv('H:\\extra\\blah', 1)
 # lemmatizer
 lemmatizer = WordNetLemmatizer()
+print('AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA')
 # number of clusters
-nclusters= 3
-# main function used to run
-clusters = cluster_sentences(example, nclusters)
-# prints the clusters and sentences
-print_clusters(clusters,example)
+nclusters= 180
 
+# main function used to run
+clusters = cluster_sentences(example1, nclusters)
+print('BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB')
+# prints the clusters and sentences
+x = print_clusters(clusters,example1)
+#print(clusters)
+
+write_file = open('H:\\extra\\meh_new.csv', 'w', newline='', encoding="latin1")
+csv_write = csv.writer(write_file)
+
+writecsv(csv_write, x)
+write_file.close()
+print('CCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCC')
